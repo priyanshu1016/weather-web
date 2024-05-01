ca5716536e1ae9cd92a1f5fb21f58f8dd54c49fc
priyanshu1016 <21evjcs071@vitj.ac.in>
2024-05-01 10:12:33 +0530

    Weather project
    its a wether web which will tell you weather of every place whererver you want.

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+function App() {
+
+  const apiKey="f56f24967aaf51182d1d4df628297c6d";
+  const [data , setData ] = useState({});
+  const [cityName , setInputCity ] = useState("");
+  const getweatherDatails =(cityName) =>{
+    if(!cityName) return;
+    const apiURL = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey; 
+    axios.get(apiURL).then((res) =>{
+      console.log("response",res.data);
+      setData(res.data);
+    }).catch((err) =>{
+      console.log("error",err);
+    })
+  }
+  const handleChangeInput =(e) =>{
+  //  console.log("value",e.target.value);
+    setInputCity(e.target.value);
+  }
+const handleSearch =() =>{
+ getweatherDatails(cityName);
+}
+
+
+  return (
+   <div className="col-md-12">
+    <div className="weatherBg">
+      <h1 className="heading">Weather app</h1>
+
+      <div className="d-grid gap-3 col-4 mt-4">
+        <input type="text" className="form-control" onChange = {handleChangeInput}/>
+        <button className="btn btn-primary" type="button" onClick={handleSearch}>search</button>
+         
+      </div>
+    </div>
+    {Object.keys(data).length > 0 && 
+    <div className="col-md-12 text-center mt-5">
+      <div className="shadow rounded weatherResultBox">
+
+    <img className="weatherIcon" src="https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png" />
+    <h5 className="weatherCity">{data?.name}</h5>
+    <h6 className="weatherTemp">{((data?.main?.temp) - 273.15).toFixed(2)}°C</h6>
+      </div>
+    </div>
+    }
+   </div>
+  );
+}
+
+export default App;
